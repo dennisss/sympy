b8d8192e6b870eb510a9bd0267e8bfea043a7038
Dennis Shtatnov <densht@gmail.com>
2014-09-14 17:02:35 -0400

    Allow latex printing with custom matrix alignment

--- sympy/printing/latex.py
+++ sympy/printing/latex.py
@@ -111,6 +111,7 @@ class LatexPrinter(Printer):
         "mat_str": None,
         "mat_delim": "[",
         "symbol_names": {},
+		"align": "c"
     }
 
     def __init__(self, settings=None):
@@ -1258,7 +1259,7 @@ class LatexPrinter(Printer):
             if self._settings['mode'] == 'inline':
                 mat_str = 'smallmatrix'
             else:
-                if (expr.cols <= 10) is True:
+                if (expr.cols <= 10) is True and self._settings['align'] == 'c':
                     mat_str = 'matrix'
                 else:
                     mat_str = 'array'
@@ -1266,7 +1267,7 @@ class LatexPrinter(Printer):
         out_str = r'\begin{%MATSTR%}%s\end{%MATSTR%}'
         out_str = out_str.replace('%MATSTR%', mat_str)
         if mat_str == 'array':
-            out_str = out_str.replace('%s', '{' + 'c'*expr.cols + '}%s')
+            out_str = out_str.replace('%s', '{' + self._settings['align']*expr.cols + '}%s')
         if self._settings['mat_delim']:
             left_delim = self._settings['mat_delim']
             right_delim = self._delim_dict[left_delim]
@@ -1669,7 +1670,7 @@ class LatexPrinter(Printer):
         return latex_result
 
     def _print_DiagramGrid(self, grid):
-        latex_result = "\\begin{array}{%s}\n" % ("c" * grid.width)
+        latex_result = "\\begin{array}{%s}\n" % (self._settings["align"] * grid.width)
 
         for i in xrange(grid.height):
             for j in xrange(grid.width):
