ed78429af306306a2a0d8d1a1cf0db473605f8ca
Dennis Shtatnov <densht@gmail.com>
2014-09-15 16:48:02 -0400

    Add LaTeX parser

---
+++ sympy/parsing/tests/test_latex.py
@@ -0,0 +1,24 @@
+from sympy.parsing.latex import parse_latex
+
+from sympy import *
+from sympy.abc import x, y, z, w, pi
+
+def test_latex_parser():
+	inputs = {
+		"x" : x,
+		"1.234": 1.234,
+		"x + y": (x + y),
+		"xy": (x*y),
+		"\\frac{x}{y}": (x/y),
+		"\\begin{matrix} 1 & 2 \\\\ 3 & 4 \\end{matrix}": Matrix([ [1,2], [3,4] ]),
+		"\\left[ \\begin{matrix} 1 & 2 \\\\ 3 & 4 \\end{matrix} \\right]": Matrix([ [1,2], [3,4] ]),
+		"( x+ y) (z + w) ": ((x+y)*(z+w)),
+		"\\sqrt[y] 2": root(2, y),
+		"\\sqrt 2x": sqrt(2)*x,
+		"x + z\\pi = y": Eq(x+z*pi, y),
+		"\\int x \mathrm{d}x": integrate(x, x)
+	}
+
+	for text, result in inputs.items():
+		print(parse_latex(text))
+		assert parse_latex(text) == result
